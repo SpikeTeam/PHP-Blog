f358b697a69a27143fddceca8962c619a9cd0fe4
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2017-03-13 09:43:51 +0200

    change the comments

--- sql-part/create_schema.sql
+++ sql-part/create_schema.sql
@@ -1,7 +1,7 @@
------- Create Schema
+-- ---- Create Schema
 CREATE SCHEMA `php_blog` DEFAULT CHARACTER SET utf8 ;
 
------- Create table User
+-- ---- Create table User
 CREATE TABLE `php_blog`.`user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(128) NOT NULL UNIQUE,
@@ -15,7 +15,7 @@ CREATE TABLE `php_blog`.`user` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
------- Create table Post
+-- ---- Create table Post
 CREATE TABLE `php_blog`.`post` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `title` VARCHAR(32) NOT NULL UNIQUE,
@@ -26,7 +26,7 @@ CREATE TABLE `php_blog`.`post` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
------- Create table Category
+-- ---- Create table Category
 CREATE TABLE `php_blog`.`category`(
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(32) NOT NULL UNIQUE
@@ -34,7 +34,7 @@ CREATE TABLE `php_blog`.`category`(
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
------- Create table Comment
+-- ---- Create table Comment
 CREATE TABLE `php_blog`.`comment`(
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `date_posted` DATETIME NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE `php_blog`.`comment`(
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
------- Create table Tag
+-- ---- Create table Tag
 CREATE TABLE `php_blog`.`tag`(
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(32) NOT NULL UNIQUE
@@ -51,7 +51,7 @@ CREATE TABLE `php_blog`.`tag`(
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
------- Add column author_id and category_id to `post`
+-- ---- Add column author_id and category_id to `post`
 USE `php_blog`;
 ALTER TABLE `post`
 ADD COLUMN `user_id` INT UNSIGNED NOT NULL;
@@ -59,34 +59,34 @@ ADD COLUMN `user_id` INT UNSIGNED NOT NULL;
 ALTER TABLE `post`
 ADD COLUMN `category_id` INT UNSIGNED NOT NULL;
 
------- Add foreign key post to user (many to one)
+-- ---- Add foreign key post to user (many to one)
 ALTER TABLE `post`
 ADD FOREIGN KEY (`user_id`)
 REFERENCES `user`(`id`);
 
------- Add foreign key post to category (many to one)
+-- ---- Add foreign key post to category (many to one)
 ALTER TABLE `post`
 ADD FOREIGN KEY (`category_id`)
 REFERENCES `category`(`id`);
 
------- Add author_id and post_id to `comment`
+-- ---- Add author_id and post_id to `comment`
 ALTER TABLE `comment`
 ADD COLUMN `user_id` INT UNSIGNED NOT NULL;
 
 ALTER TABLE `comment`
 ADD COLUMN `post_id` INT UNSIGNED NOT NULL;
 
------- Add foreign key comment to user (many to one)
+-- ---- Add foreign key comment to user (many to one)
 ALTER TABLE `comment`
 ADD FOREIGN KEY (`user_id`)
 REFERENCES `user`(`id`);
 
------ Add foreign key comment to post (many to one)
+-- --- Add foreign key comment to post (many to one)
 ALTER TABLE `comment`
 ADD FOREIGN KEY (`post_id`)
 REFERENCES `post`(`id`);
 
------ Create buffer table `Post_Tag` connect post and tag (many to many)
+-- --- Create buffer table `Post_Tag` connect post and tag (many to many)
 CREATE TABLE `POST_TAG`(
   `post_id` INT UNSIGNED NOT NULL,
   `tag_id` INT UNSIGNED NOT NULL
@@ -94,7 +94,7 @@ CREATE TABLE `POST_TAG`(
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
------- Add foreign keys to post and tag
+-- ---- Add foreign keys to post and tag
 ALTER TABLE `post_tag`
 ADD FOREIGN KEY (`post_id`)
 REFERENCES `post`(`id`);
